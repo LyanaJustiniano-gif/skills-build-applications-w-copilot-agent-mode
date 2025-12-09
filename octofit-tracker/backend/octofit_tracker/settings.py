f9729d62da67011acc918ca5c1e66dd76c3f27ac
LyanaJustiniano-gif <lyanajustiniano@gmail.com>
2025-12-09 01:01:09 +0000

    Agregar configuración inicial de URLs para la aplicación Django

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-u)x+ri@-^t!juf%4-$36_u+j&a1q&#-*w%(2ekuqwwx-25q2l-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
