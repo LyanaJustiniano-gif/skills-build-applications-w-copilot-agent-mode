ccf140057ee672c3226a2e497526e70d59a473cd
LyanaJustiniano-gif <lyanajustiniano@gmail.com>
2025-12-09 00:52:56 +0000

    Agregar comando para poblar la base de datos octofit_db con datos de prueba

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Borrar datos existentes
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Crear equipos
+        marvel = Team.objects.create(name='marvel')
+        dc = Team.objects.create(name='dc')
+
+        # Crear usuarios
+        ironman = User.objects.create(email='ironman@marvel.com', name='Iron Man', team='marvel')
+        captain = User.objects.create(email='captain@marvel.com', name='Captain America', team='marvel')
+        batman = User.objects.create(email='batman@dc.com', name='Batman', team='dc')
+        superman = User.objects.create(email='superman@dc.com', name='Superman', team='dc')
+
+        # Crear actividades
+        Activity.objects.create(user='Iron Man', type='run', duration=30)
+        Activity.objects.create(user='Captain America', type='cycle', duration=45)
+        Activity.objects.create(user='Batman', type='swim', duration=25)
+        Activity.objects.create(user='Superman', type='run', duration=50)
+
+        # Crear leaderboard
+        Leaderboard.objects.create(team='marvel', points=75)
+        Leaderboard.objects.create(team='dc', points=75)
+
+        # Crear workouts
+        Workout.objects.create(name='Pushups', description='Do 20 pushups')
+        Workout.objects.create(name='Squats', description='Do 30 squats')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data'))
